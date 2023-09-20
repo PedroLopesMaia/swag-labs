79a27b8d67cfc70f5041eb32e6967b1060f7de3e
Pedro Lopes <pedrolopes_maia@hotmail.com>
2023-09-20 16:12:34 -0300

    Cenários de login concluídos.

--- src/test/java/stepDefinitions/Login_Steps.java
+++ src/test/java/stepDefinitions/Login_Steps.java
@@ -22,19 +22,17 @@ public class Login_Steps extends Base_PO {
     public void i_access_the_login_page() {
         navigateTo_URL("https://www.saucedemo.com/");
     }
-    @When("I enter the correct username")
-    public void i_enter_the_correct_username() {
-        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
-    }
-    @And("I enter the correct password")
-    public void i_enter_the_correct_password() {
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+
+    @When("I enter a username {word}")
+    public void i_enter_a_correct_username(String username) {
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys(username);
     }
 
-    @And("I enter a wrong password")
-    public void i_enter_a_wrong_password() {
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("senhaerrada");
+    @And("I enter a password {word}")
+    public void i_enter_a_password(String password) {
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
     }
+
 
     @And("I click the login button")
     public void i_click_the_login_button() {
@@ -47,15 +45,37 @@ public class Login_Steps extends Base_PO {
         Assert.assertEquals(URL_Main_Page, "https://www.saucedemo.com/inventory.html");
     }
 
-    @Then("I should be presented with a wrong credentials error message")
-    public void i_should_be_presented_with_a_wrong_credentials_error_message() {
+    @Then("I am presented with a wrong credentials error message")
+    public void i_am_presented_with_a_wrong_credentials_error_message() {
         WebElement error_message = driver.findElement(By.className("error-message-container"));
         Assert.assertEquals(error_message.getText(), "Epic sadface: Username and password do not match any user in this service");
     }
 
-    @Then("I should be presented with a empty fields error message")
-    public void i_should_be_presented_with_a_empty_fields_error_message() {
+    @Then("I am presented with a empty fields error message")
+    public void i_am_presented_with_a_empty_fields_error_message() {
         WebElement error_message = driver.findElement(By.className("error-message-container"));
         Assert.assertEquals(error_message.getText(), "Epic sadface: Username is required");
+    }
+
+    @Then("I should be able to click on the error message close button")
+    public void i_should_be_able_to_click_on_the_error_message_close_button() {
+        WebElement error_button = driver.findElement(By.className("error-button"));
+        this.waitForWebElementAndClick(error_button);
+    }
+
+    @When("I enter a blocked username")
+    public void i_enter_a_blocked_username() {
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("locked_out_user");
+    }
+
+    @And("I enter the correct password")
+    public void i_enter_the_correct_password() {
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+    }
+
+    @And("I am presented with a blocked user error message")
+    public void i_am_presented_with_a_blocked_user_error_message() {
+        WebElement error_message = driver.findElement(By.className("error-message-container"));
+        Assert.assertEquals(error_message.getText(), "Epic sadface: Sorry, this user has been locked out.");
     }
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -5,18 +5,38 @@ Feature:Swag Labs - Login
   Background:
     Given I access the login page
 
-  Scenario: Validate Successful Login - Correct password
-    When I enter the correct username
-    And I enter the correct password
-    And I click the login button
-    Then I should be presented with the main page
-
-  Scenario: Validate Unsuccessful Login - Wrong password
-    When I enter the correct username
-    And I enter a wrong password
-    And I click the login button
-    Then I should be presented with a wrong credentials error message
-
   Scenario: Validate Unsuccessful Login - Empty fields
     When I click the login button
-    Then I should be presented with a empty fields error message
+    And I am presented with a empty fields error message
+    Then I should be able to click on the error message close button
+
+  Scenario: Validate Locked Out User
+    When I enter a blocked username
+    And I enter the correct password
+    And I click the login button
+    And I am presented with a blocked user error message
+    Then I should be able to click on the error message close button
+
+  Scenario Outline: Validate - Successful Login
+    When I enter a username <username>
+    And I enter a password <password>
+    And I click the login button
+    Then I should be presented with the main page
+    Examples:
+      | username                | password     |
+      | standard_user           | secret_sauce |
+      | problem_user            | secret_sauce |
+      | performance_glitch_user | secret_sauce |
+
+  Scenario Outline: Validate - Unsuccessful Login
+    When I enter a username <username>
+    And I enter a password <password>
+    And I click the login button
+    And I am presented with a wrong credentials error message
+    Then I should be able to click on the error message close button
+
+    Examples:
+      | username      | password     |
+      | usuario       | secret_sauce |
+      | standard_user | senha_errada |
+      | usuario       | senha_errada |
